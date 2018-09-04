340c3cbded57035b2ea3a7bad9bf60f9ed084f6f
Flavio Magela <flavio.mrsantos@gmail.com>
2018-09-04 18:02:17 -0300

    Programção defensiva - Campo Data - maior segurança para imutabilidade - Boas práticas de programação

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,16 +1,19 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-    this._data = data;
+
+    this._data = new Date(data.getTime()); // maior segurança para imutabilidade - vc instancia uma nova data. Boas práticas de programação
     this._quantidade = quantidade;
     this._valor = valor;
+    Object.freeze(this);
   }
+
   // Função criada dentro de uma Classe, chama-se de Método...
   // Funcção criada fora de uma Classe, chama-se de Função mesmo...
   get volume(){ //propriedade de leitura - Get - não pode atribuir um valor
     return this._quantidade * this._valor;
   }
   get data() {//propriedade de leitura - Get - não pode atribuir um valor
-    return this._data;
+    return new Date(this._data.getTime());
   }
   get quantidade() {//propriedade de leitura - Get - não pode atribuir um valor
     return this._quantidade;
