060641ddc53e05b6b2085edce0ec0693d603407b
Flavio Magela <flavio.mrsantos@gmail.com>
2018-09-06 14:55:50 -0300

    Melhores práticas: Evitando o DOM ser percorrido várias vezes... Metodo Constructor utilizando o This.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,21 @@
 class NegociacaoController{
+
+  constructor(){
+
+    let $ = document.querySelector.bind(document) // metodo .bind() mantem a associação com o document (pai) como objeto e não como variável...
+    this.inputData = $('#data'); // utilizando o $ como objeto .bind()
+    this.inputQuantidade = $('#quantidade');
+    this.inputValor = $('#valor');
+  }
+
   adiciona(event){
+
     event.preventDefault();
     alert("Ação realizada com sucesso.");
 
-    let $ = document.querySelector.bind(document) // metodo .bind() mantem a associação com o document (pai) como objeto e não como variável...
-    let inputData = $('#data'); // utilizando o $ como objeto .bind()
-    let inputQuantidade = $('#quantidade');
-    let inputValor = $('#valor');
-
-    console.log(inputData.value);
-    console.log(inputQuantidade.value);
-    console.log(inputValor.value);
+    console.log(this.inputData.value);
+    console.log(this.inputQuantidade.value);
+    console.log(this.inputValor.value);
   }
 
 }
